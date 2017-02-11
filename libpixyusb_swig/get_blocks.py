f141decb56b5f0816e7b6d5fbc2c6bd3db79300c
Vivek Malneedi <vivekmalneedi@gmail.com>
2017-02-11 14:33:18 -0500

    formatting + send multiple blocks at once

--- libpixyusb_swig/get_blocks.py
+++ libpixyusb_swig/get_blocks.py
@@ -3,12 +3,13 @@ from ctypes import *
 import socket
 from subprocess import call
 import time
-import sys
+
 
 def log(data):
     f = open("log.txt", "w")
     f.write(data)
     f.close()
+
 
 log("started running")
 call('sudo ./hello_pixy', shell=True)
@@ -18,21 +19,18 @@ time.sleep(0.1)
 
 # Initialize Pixy Interpreter thread #
 pixy_init()
-print ("Initialized Pixy")
+print("Initialized Pixy")
 log("Initialized Pixy")
-class Blocks (Structure):
-  _fields_ = [ ("type", c_uint),
-               ("signature", c_uint),
-               ("x", c_uint),
-               ("y", c_uint),
-               ("width", c_uint),
-               ("height", c_uint),
-               ("angle", c_uint) ]
+
+
+class Blocks(Structure):
+    _fields_ = [("type", c_uint), ("signature", c_uint), ("x", c_uint),
+                ("y", c_uint), ("width", c_uint), ("height", c_uint),
+                ("angle", c_uint)]
+
 
 blocks = BlockArray(100)
-frame  = 0
-
-# Wait for blocks #
+frame = 0
 
 HOST = ''
 PORT = 5800
@@ -45,29 +43,36 @@ print("Got connection from", addr)
 log("Got connection from")
 test = pixy_get_blocks(100, blocks)
 log("received data")
+
+# Wait for blocks #
 while 1:
 
-  count = pixy_get_blocks(100, blocks)
-  if count > 0:
-    # Blocks found #
-    print 'frame %3d:' % (frame)
-    frame = frame + 1
-    for index in range (0, count):
-      print '[FRAME=%3d BLOCK_TYPE=%d SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (frame, blocks[index].type, blocks[index].signature, blocks[index].x, blocks[index].y, blocks[index].width, blocks[index].height)
-      output = []
-      output.append(str(frame))
-      output.append(str(blocks[index].type))
-      output.append(str(blocks[index].signature))
-      output.append(str(blocks[index].x))
-      output.append(str(blocks[index].y))
-      output.append(str(blocks[index].width))
-      output.append(str(blocks[index].height))
-      try:
-        conn.send(" ".join(output) + "\n")
-      except socket.error:
-        conn.close()
-        soc.listen(5)
-        conn, addr = soc.accept()
-        # print("Got connection from", addr)
-        
-        
+    count = pixy_get_blocks(100, blocks)
+    if count > 0:
+        # Blocks found #
+        print 'frame %3d:' % (frame)
+        frame = frame + 1
+        output = []
+        for index in range(0, count):
+            detected = []
+            print '[FRAME=%3d BLOCK_TYPE=%d SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (
+                frame, blocks[index].type, blocks[index].signature,
+                blocks[index].x, blocks[index].y, blocks[index].width,
+                blocks[index].height)
+            detected.append(str(frame))
+            detected.append(str(blocks[index].type))
+            detected.append(str(blocks[index].signature))
+            detected.append(str(blocks[index].x))
+            detected.append(str(blocks[index].y))
+            detected.append(str(blocks[index].width))
+            detected.append(str(blocks[index].height))
+            output.append(detected)
+        toSend = ""
+        for index in output:
+            toSend += " ".join(index) + ","
+        try:
+            conn.send(toSend + "\n")
+        except socket.error:
+            conn.close()
+            soc.listen(5)
+            conn, addr = soc.accept()
